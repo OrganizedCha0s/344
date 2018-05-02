ae198074ef02b5bc67fd6d569728c24fa51eb32e
OrganizedCha0s <38442036+OrganizedCha0s@users.noreply.github.com>
2018-05-01 19:37:54 -0500

    Update Final Project SQL.sql

--- Final Project SQL.sql
+++ Final Project SQL.sql
@@ -392,3 +392,286 @@ CONSTRAINT parguar_stuID_pk PRIMARY KEY (stuID),
 CONSTRAINT parguar_stuID_fk FOREIGN KEY (stuID) REFERENCES per (perID), 
 CONSTRAINT parguar_parID_fk FOREIGN KEY (parID) REFERENCES per (perID),
 CONSTRAINT sign_cc CHECK ((sign = 'Y') OR (sign = 'N')));
+
+					   
+					   
+--INSERT STATEMENTS
+--PERSON
+INSERT INTO pers 
+  VALUES (5001, 'Noah', 'Wetzel','James');
+  
+INSERT INTO pers
+  VALUES (5002, 'Jordan', 'Jansen','Fredrick'); 
+  
+INSERT INTO pers 
+  VALUES (5003, 'Ian', 'Wetzel','Charles');
+  
+INSERT INTO pers 
+  VALUES (5004, 'Justin', 'Lorentz','Allen');  
+  
+INSERT INTO pers 
+  VALUES (5005, 'Rachel', 'Stout','Marie');
+  
+INSERT INTO pers 
+  VALUES (5006, 'Erin', 'Wetzel','Charlene');
+
+INSERT INTO pers 
+  VALUES (5007, 'Sunny', '', 'Chen');
+  
+INSERT INTO pers 
+  VALUES (5008, 'Jacob', 'Maurer','Peter');
+
+INSERT INTO pers 
+  VALUES (5009, 'Yong Sheng', '', 'Lai');
+
+INSERT INTO pers 
+  VALUES (5010, 'Mark', 'Smith', 'Brewer');
+
+INSERT INTO pers 
+  VALUES (5011, 'Sarah', 'Adam', 'Lee');
+
+INSERT INTO pers 
+  VALUES (5012, 'Patricia', 'Jacobsen' ,'Pweter');
+
+INSERT INTO pers 
+  VALUES (5013, 'Adam', 'Martin' ,'Jake');
+
+INSERT INTO pers 
+  VALUES (5014, 'Eli', '','Behling');
+
+INSERT INTO pers 
+  VALUES (5015, 'Sam', 'Paul','Smith');
+
+INSERT INTO pers 
+  VALUES (5016, 'Sandy', '','Wong');
+  
+INSERT INTO pers 
+  VALUES (5017, 'John', 'Jacobs','James');
+  
+INSERT INTO pers 
+  VALUES (5018, 'Adam', 'Seth','Johnson');
+  
+INSERT INTO pers 
+  VALUES (5019, 'Sam', 'David','Heath');
+  
+INSERT INTO pers 
+  VALUES (5020, 'Samantha', 'Erin','Dunbar');
+
+INSERT INTO pers 
+  VALUES (5021, 'Eli', '','Manning');
+--DEMOGRAPHIC 
+
+INSERT INTO dem
+  VALUES (2001, 'M', 16-OCT-1996);
+  
+INSERT INTO dem
+  VALUES (2002, 'M', 01-JAN-1950);
+  
+INSERT INTO dem
+  VALUES (2003, 'M', 20-APR-1994);
+  
+INSERT INTO dem
+  VALUES (2004, 'M', 25-APR-1997);  
+  
+INSERT INTO dem
+  VALUES (2005, 'F', 15-JUN-1997);  
+  
+INSERT INTO dem
+  VALUES (2006, 'F', 30-SEP-1998);
+  
+ INSERT INTO dem
+  VALUES (2007, 'F', 25-APR-1997); -- check date
+  
+INSERT INTO dem
+  VALUES (2008, 'M', 28-JUN-1945); 
+	
+INSERT INTO dem
+  VALUES (2009, 'M', 30-APR-1992);
+	
+INSERT INTO dem
+  VALUES (2010, 'F', 21-JUL-1992);
+	
+INSERT INTO dem
+  VALUES (2011, 'F', 10-APR-1994);	
+  
+--EMAIL  
+INSERT INTO email
+  VALUES ('WETZELNJ5708@uwec.edu', 'WETZELNJ5708', 'uwec.edu')
+  
+INSERT INTO email
+  VALUES ('jordan@uwec.edu', 'j', 'uwec.edu') --
+  
+INSERT INTO email
+  VALUES ('ianwtz@gmail.com', 'ianwtz', 'gmail.com')
+  
+INSERT INTO email
+  VALUES ('LORENTJA6352@uwec.edu', 'LORENTJA6352', 'uwec.edu')  
+  
+INSERT INTO email
+  VALUES ('rachel@uwec.edu', 'r', 'uwec.edu')     --
+  
+INSERT INTO email
+  VALUES ('erin123@gmail.com', 'erin123', 'gmail.com')
+  
+INSERT INTO email
+  VALUES ('sunny@uwec.edu', 's', 'uwec.edu')
+  
+INSERT INTO email
+  VALUES ('jake@uwec.edu', 'jake', 'uwec.edu')   --
+  
+--PHONE DO WE NEED TO ADD PERSON ID?? 
+
+INSERT INTO phn
+  VALUES ('10001', 'C', '1', '920', 8583994) --Fix data type in create table
+  
+INSERT INTO phn
+  VALUES ('10002', 'C', '1', '715', 7652134)  
+  
+INSERT INTO phn
+  VALUES ('10003', 'C', '1', '920', 4562177)  
+  
+INSERT INTO phn
+  VALUES ('10004', 'W', '1', '414', 8865432) 
+  
+INSERT INTO phn
+  VALUES ('10005', 'C', '1', '952', 7775544) 
+  
+INSERT INTO phn
+  VALUES ('10006', 'W', '1', '920', 3435612)  
+  
+INSERT INTO phn
+  VALUES ('10007', 'C', '1', '920', 8583994) 
+  
+INSERT INTO phn
+  VALUES ('10008', 'C', '1', '715', 7785656) 
+  
+  
+--ADDRESS
+INSERT INTO addr
+  VALUES (5001, 'Niagara', 'Eau Claire', 54703);
+  
+INSERT INTO addr
+  VALUES (5002, 'State', 'Madison', 53703);
+  
+INSERT INTO addr
+  VALUES (5003, 'Sunset', 'Neenah', 54956);
+  
+INSERT INTO addr
+  VALUES (5004, 'Allen', 'Chippewa Falls', 54729);  
+
+INSERT INTO addr
+  VALUES (5005, 'Gilbert', 'Eau Claire', 54701);
+  
+INSERT INTO addr
+  VALUES (5006, 'Hartford', 'Milwaukee', 53211); 
+  
+INSERT INTO addr
+  VALUES (5007, 'Gartfield', 'Eau Claire', 54701);
+  
+INSERT INTO addr
+  VALUES (5008, 'University', 'Eau Claire', 54701);
+   
+--HOSPITAL
+
+INSERT INTO hos 
+  VALUES (stuID, hosID, hosname, hosprimary?, hosphn)
+  
+--STUDENT (stuID,stuorgID, student year inschool jun sen, student lunch y/n, student retained, stu grade comments, stu grades sat?, stu homework,stuservID)
+INSERT INTO stu
+  VALUES (2001, 6001, 'Junior', 'Y', 'N', ' ', 'Y', 'Math', 7001);
+  
+INSERT INTO stu
+  VALUES (2002, 6002, 'Freshman', 'N', 'N', 'Would like to see extra work on sciences', 'N', 'Science', 7002); 
+	
+INSERT INTO stu
+  VALUES (2003, 6003, 'Sophmore', 'N', 'Y', 'Would like to see extra time spend on gym/excercise', 'N', 'Gym', 7003); 	
+
+INSERT INTO stu
+  VALUES (2004, 6004, 'Senior', 'Y', 'N', ' ', 'Y', ' ', 7004); 
+
+INSERT INTO stu
+  VALUES (2005, 6005, 'Freshman', 'N', 'N', 'Needs extra time with math', 'Y', 'Math', 7005);
+
+INSERT INTO stu
+  VALUES (2006, 6006, 'Freshman', 'N', 'N', 'Would like to see extra time with science', 'N', 'Science', 7006);
+	
+INSERT INTO stu
+  VALUES (2007, 6007, 'Sophmore', 'N', 'Y', '', 'Y', ' ', 7007); 	
+
+INSERT INTO stu
+  VALUES (2008, 6008, 'Junior', 'Y', 'N', 'Needs help with social studies', 'N', 'Social studies', 7008);
+
+INSERT INTO stu
+  VALUES (2009, 6009, 'Freshman', 'N', 'N', '', 'Y', 'Science', 7009);
+
+INSERT INTO stu
+  VALUES (2010, 6010, 'Senior', 'N', 'N', 'Needs help with nutrition education', 'Y', '', 7010);
+
+	
+
+--INSERT EMERGENCY CONTACT
+INSERT INTO emg
+	VALUES (1010, 5001, 'Brother', 'Y', 'Y', 'N');
+
+INSERT INTO emg
+	VALUES (1011, 5002, Friend, 'N', 'Y', 'N');
+
+INSERT INTO emg
+	VALUES (1012, 5003, Colleague, 'Y', 'N', 'Y');
+
+INSERT INTO emg
+	VALUES (1013, 5004, Cousin, 'Y', 'Y', 'N');
+
+INSERT INTO emg
+	VALUES (1014, 5005, Sister, 'Y', 'N', 'Y');
+
+INSERT INTO emg
+	VALUES (1015, 5006, Brother, 'N', 'N', 'Y');
+
+INSERT INTO emg
+	VALUES (1016, 5007, Wife, 'Y', 'Y', 'N');
+
+--Income Table --FIX ID
+INSERT INTO inc 
+VALUES ( ?,5, 59000, N, father);
+
+INSERT INTO inc 
+VALUES ( ?,4, 69000, Y, father);
+
+INSERT INTO inc 
+VALUES ( ?,6, 120000, N,  Both Parents);
+
+INSERT INTO inc 
+VALUES ( ?,2, 88000, N, Mother);
+
+INSERT INTO inc 
+VALUES ( ?,3, 70000, N,  Guardian);
+
+INSERT INTO inc 
+VALUES ( ?,2, 30000, N,  Grandparent);
+
+INSERT INTO inc 
+VALUES ( ?,5, 90000, N,  Foster Parent);
+
+INSERT INTO inc 
+VALUES ( ?,7, 100000, N,  Relative);
+
+	
+--ATTENDANCE TABLE (2001, attendance date, check in date, check out date)
+INSERT INTO att	
+	VALUES (2001, 28-APR-2018, 28-APR-2018, 30-APR-2018);
+	
+INSERT INTO att	
+	VALUES (2002, 17-MAR-2018, 10-MAR-2018, 30-APR-2018);	
+
+INSERT INTO att	
+	VALUES (2003, 04-FEB-2018, 02-JAN-2018, 07-FEB-2018);
+	
+INSERT INTO att	
+	VALUES (2004, 28-OCT-2017, 14-OCT-2017, 30-OCT-2017);	
+	
+INSERT INTO att	
+	VALUES (2005, 13-SEP-2017, 20-AUG-2016, 30-APR-2018);
+	
+INSERT INTO att	
+	VALUES (2006, 22-NOV-2018, 01-NOV-2016, 23-NOV-2017);						   
